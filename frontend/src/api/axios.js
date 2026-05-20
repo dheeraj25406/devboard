15c14216ca1be30f6d4652d92af8f93496f4f0a8
Dheeraj Alamuri <adheerajhyd@gmail.com>
2026-05-20 14:15:33 +0530

    fix api url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -5,7 +5,7 @@ const API_BASE_URL=
   'http://localhost:8000/api';
 
 const api=axios.create({
-  baseURL:API_BASE_URL,
+  baseURL:https://devboard-backend-uoj6.onrender.com/api,
   headers:{
     'Content-Type':'application/json'
   }
